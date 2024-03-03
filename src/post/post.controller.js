0f7d5202428db1f3088d5f63e8bb430e1d844ee7
oskcarsv <oskcarqsv@gmail.com>
2024-03-03 01:42:00 -0600

    fixed updatePost

--- src/post/post.controller.js
+++ src/post/post.controller.js
@@ -51,28 +51,36 @@ export const createPost = async (req, res) => {
 export const updatePost = async (req, res) => {
     const { id } = req.params;
     const { _id, ...rest } = req.body;
-
+  
     const userId = req.usuario._id;
-
-    const post = await Post.findById(id);
-
-    if (post.user.toString() !== req.usuario._id.toString()) {
-        return res.status(403).json({
-            msg: "Access denied, only the owner of the post can edit this post"
-        });
-    }
-
+  
     const user = await User.findById(userId);
-
+  
+    const post = await Post.findById(id);
+  
+    if (post.user.toString() !== req.usuario._id.toString()) {
+      return res.status(403).json({
+        msg: "Access denied, only the owner of the post can edit this post",
+      });
+    }
+  
+    const updatedPost = await Post.findByIdAndUpdate(id, { ...rest }, { new: true });
+  
+    if (!updatedPost) {
+      return res.status(404).json({
+        msg: "Post not found",
+      });
+    }
+  
     res.status(200).json({
-        msg: "Post updated successfully",
-        post: {
-            ...post._doc,
-            user: user.nombre,
-        },
+      msg: "Post updated successfully",
+      post: {
+        ...updatedPost._doc,
+        user: user.nombre,
+      },
     });
-}
-
+  };
+    
 
 export const deletePost = async (req, res) => {
     const { id } = req.params;
